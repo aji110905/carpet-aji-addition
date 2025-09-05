990187ae7049b852bc25fa57319a09d2720eaffc
aji110905 <2442733838@qq.com>
2025-09-05 14:58:04 +0800

    添加规则可合成龙蛋

--- src/main/java/aji/carpetajiaddition/validators/RecipeRule/RecipeRuleValidator.java
+++ src/main/java/aji/carpetajiaddition/validators/RecipeRule/RecipeRuleValidator.java
@@ -1,46 +1,8 @@
 package aji.carpetajiaddition.validators.RecipeRule;
 
 import carpet.api.settings.Validator;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public abstract class RecipeRuleValidator<T> extends Validator<T> {
-    public Map<String, JsonObject> readRecipeFiles(String folderName) {
-        Map<String, JsonObject> fileMap = new HashMap<>();
-        try {
-            String resourcePath = "assets/carpetajiaddition/RecipesTweak/" + folderName;
-            URL resourceUrl = RecipeRuleValidator.class.getClassLoader().getResource(resourcePath);
-            if (resourceUrl == null) return null;
-            try (Stream<Path> stream = Files.list(Paths.get(resourceUrl.toURI()))) {
-                stream.forEach(path -> {
-                    String fileName = path.getFileName().toString();
-                    String filePath = resourcePath + "/" + fileName;
-                    try (InputStream inputStream = RecipeRuleValidator.class.getClassLoader().getResourceAsStream(filePath)) {
-                        if (inputStream != null) {
-                            String content = new String(inputStream.readAllBytes());
-                            JsonObject jsonObject = JsonParser.parseString(content).getAsJsonObject();
-                            fileMap.put(fileName, jsonObject);
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-        } catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return fileMap;
-    }
+    public abstract Map readRecipeFiles(String folderName);
 }
--- src/main/java/aji/carpetajiaddition/validators/RecipeRule/SimpleRecipeRuleValidator.java
+++ src/main/java/aji/carpetajiaddition/validators/RecipeRule/SimpleRecipeRuleValidator.java
@@ -2,13 +2,21 @@ package aji.carpetajiaddition.validators.RecipeRule;
 
 import carpet.api.settings.CarpetRule;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.WorldSavePath;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class SimpleRecipeRuleValidator<T> extends RecipeRuleValidator<T> {
     private String targetPath;
@@ -37,7 +45,7 @@ public class SimpleRecipeRuleValidator<T> extends RecipeRuleValidator<T> {
     }
 
     public void loadRecipe(String ruleName){
-        Map<String, JsonObject> recipeFiles = super.readRecipeFiles(ruleName);
+        Map<String, JsonObject> recipeFiles = readRecipeFiles(ruleName);
         if (recipeFiles == null) return;
         recipeFiles.forEach((fileName, jsonObject) -> {
             File file = new File(targetPath, fileName);
@@ -59,5 +67,34 @@ public class SimpleRecipeRuleValidator<T> extends RecipeRuleValidator<T> {
                 file.delete();
             }
         });
+    }
+
+    @Override
+    public Map<String, JsonObject> readRecipeFiles(String folderName) {
+        Map<String, JsonObject> fileMap = new HashMap<>();
+        try {
+            String resourcePath = "assets/carpetajiaddition/RecipesTweak/" + folderName;
+            URL resourceUrl = RecipeRuleValidator.class.getClassLoader().getResource(resourcePath);
+            if (resourceUrl == null) return null;
+            try (Stream<Path> stream = Files.list(Paths.get(resourceUrl.toURI()))) {
+                stream.forEach(path -> {
+                    String fileName = path.getFileName().toString();
+                    String filePath = resourcePath + "/" + fileName;
+                    try (InputStream inputStream = RecipeRuleValidator.class.getClassLoader().getResourceAsStream(filePath)) {
+                        if (inputStream != null) {
+                            String content = new String(inputStream.readAllBytes());
+                            JsonObject jsonObject = JsonParser.parseString(content).getAsJsonObject();
+                            fileMap.put(fileName, jsonObject);
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return fileMap;
     }
 }
