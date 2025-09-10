6e39c87a90cda29019009fa1e7d252bd8c7c66e7
aji110905 <2442733838@qq.com>
2025-09-10 17:48:44 +0800

    让规则分类的名字更符合命名规范

--- src/main/java/aji/carpetajiaddition/setting/RuleCategory.java
+++ src/main/java/aji/carpetajiaddition/setting/RuleCategory.java
@@ -2,5 +2,5 @@ package aji.carpetajiaddition.setting;
 
 public class RuleCategory {
     public static final String CAA = "CAA";
-    public static final String RECIPE_RULE = "recipe_rule";
+    public static final String RECIPE = "recipe";
 }
