5dd6001a922877c7992982f6ae7ff312c511ea95
aji110905 <2442733838@qq.com>
2025-09-10 15:22:52 +0800

    优化获取版本号的方式

--- src/main/java/aji/carpetajiaddition/CarpetAjiAdditionMod.java
+++ src/main/java/aji/carpetajiaddition/CarpetAjiAdditionMod.java
@@ -3,6 +3,7 @@ package aji.carpetajiaddition;
 import carpet.CarpetServer;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class CarpetAjiAdditionMod implements ModInitializer {
 	public static final String MOD_ID = "carpetajiaddition";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().getMetadata().getVersion().toString();
     public static MinecraftServer minecraftServer;
 
 	@Override
