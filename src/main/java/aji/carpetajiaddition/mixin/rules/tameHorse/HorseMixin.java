d752799a542ff987636f751e9f7f012f7af29943
aji110905 <2442733838@qq.com>
2025-09-12 14:45:13 +0800

    让mixin类成为符合规范的抽象类

--- src/main/java/aji/carpetajiaddition/mixin/rules/tameHorse/HorseMixin.java
+++ src/main/java/aji/carpetajiaddition/mixin/rules/tameHorse/HorseMixin.java
@@ -1,14 +1,23 @@
 package aji.carpetajiaddition.mixin.rules.tameHorse;
 
 import aji.carpetajiaddition.CarpetAjiAdditionSettings;
+import net.minecraft.entity.*;
 import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.inventory.InventoryChangedListener;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractHorseEntity.class)
-public class HorseMixin {
+public abstract class HorseMixin extends AnimalEntity implements InventoryChangedListener, RideableInventory, Tameable, JumpingMount, Saddleable {
+
+    protected HorseMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
     @Inject(method = "getTemper", at = @At("HEAD"), cancellable = true)
     public void getTemper(CallbackInfoReturnable<Integer> cir) {
         if (!CarpetAjiAdditionSettings.tameHorse) return;
